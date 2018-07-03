f0af195cbe7df046c402125485039772931fe3ec
Yuxiang Lin <lin.2453@osu.edu>
2018-07-03 15:13:09 -0400

    Solve repeat input problem, update view/index for user input

--- js/index.js
+++ js/index.js
@@ -11,27 +11,29 @@ katex.render("=", equal);
 katex.render("\\pm",reverse)
 
 var clearBtn = document.getElementById('clear-button');
-clearBtn.addEventListener('click', function(){
-  while (this.nextElementSibling.firstChild) {
-      this.nextElementSibling.removeChild(this.nextElementSibling.firstChild);
-  }
-}, false);
+clearBtn.addEventListener("click",clearHistory,false);
 
-var removeBtn = document.getElementsByClassName('remove-button');
+//clearBtn.addEventListener('click', function(){
+//while (this.nextElementSibling.firstChild) {
+//    this.nextElementSibling.removeChild(this.nextElementSibling.firstChild);
+//}
+//}, false);
 
-for (var i = 0; i < removeBtn.length; i++) {
-  removeBtn[i].addEventListener('click', function(){
-  this.parentNode.parentNode.removeChild(this.parentNode);
-}, false);
-};
-
-var backBtn = document.getElementsByClassName('fa-backspace');
-backBtn[0].addEventListener('click', function(){
-  var equation = document.getElementById("equation-container");
-  if (equation.value != undefined){
-    equation.value= equation.value.substring(0, equation.value.length - 1);
-    if (equation.value==''){
-        equation.value=0;
-    }
-  }
-}, false);
+//
+//var removeBtn = document.getElementsByClassName('remove-button');
+//for (var i = 0; i < removeBtn.length; i++) {
+//removeBtn[i].addEventListener('click', function(){
+//this.parentNode.parentNode.removeChild(this.parentNode);
+//}, false);
+//};
+//
+//var backBtn = document.getElementsByClassName('fa-backspace');
+//backBtn[0].addEventListener('click', function(){
+//var equation = document.getElementById("equation-container");
+//if (equation.value != undefined){
+//  equation.value= equation.value.substring(0, equation.value.length - 1);
+//  if (equation.value==''){
+//      equation.value=0;
+//  }
+//}
+//}, false);
--- js/inputListener.js
+++ js/inputListener.js
@@ -13,14 +13,21 @@ var CHAR_CODE = [16, 43, 45, 56, 42, 47, 40, 41, 69, 13, 61, 8, 46, 37, 94, 8730
 // Require: N/A
 // Update: equation, #current-input
 // Return: N/A
-function getInput(){
-  update("0");
-  handleMemory();
-  document.addEventListener("keypress", keyboardInput, false);
-  var buttons = document.getElementsByClassName("keyboard-item");
-  for (i = 5; i < buttons.length; i++){
-    buttons[i].addEventListener("click", handleInput);
-  }
+function getInput() {
+	update("0");
+	handleMemory();
+	document.addEventListener("keypress", keyboardInput, true);
+	var equationContainer = document.getElementById('equation-container');
+	equationContainer.addEventListener("focusin", function(event) {
+		document.removeEventListener("keypress", keyboardInput, true);
+	})
+	equationContainer.addEventListener("focusout", function(event) {
+		document.addEventListener("keypress", keyboardInput, true);
+	})
+	var buttons = document.getElementsByClassName("keyboard-item");
+	for(i = 5; i < buttons.length; i++) {
+		buttons[i].addEventListener("click", handleInput);
+	}
 }
 
 // Author: Gail Chen
@@ -30,8 +37,8 @@ function getInput(){
 // Require: N/A
 // Update: equation, #current-input
 // Return: N/A
-function handleInput(){
-  printToScreen(this.getAttribute("name"));
+function handleInput() {
+	printToScreen(this.getAttribute("name"));
 }
 
 // Author: Gail Chen
@@ -42,61 +49,61 @@ function handleInput(){
 // Update: equation, #current-input
 // Return: N/A
 function keyboardInput(event) {
-  var keyCode = event.which || event.keyCode;
-  var input;
-  if (keyCode != 0) {
-    if (!((keyCode <= 57) && (keyCode >= 48)) && !CHAR_CODE.includes(keyCode)) {
-      event.preventDefault();
-    }else {
-      if((keyCode <= 57) && (keyCode >= 48)){
-        input = String.fromCharCode(keyCode);
-      } else {
-          switch(keyCode){
-            case 43:
-              input = "+";
-              break;
-            case 45:
-              input = "-";
-              break;
-            case 42:
-              input = "×";
-              break;
-            case 47:
-              input = "÷";
-              break;
-            case 40:
-              input = "(";
-              break;
-            case 41:
-              input = ")";
-              break;
-            case 69:
-              input = "E";
-              break;
-            case 13: //enter
-            case 61:
-              input = "=";
-              break;
-            case 8: // backspace
-              input = "<-";
-              break;
-            case 46:
-              input = ".";
-              break;
-            case 37:
-              input = "%";
-              break;
-            case 94:
-              input = "^";
-              break;
-            case 8730:
-              input = "√(";
-              break;
-          }
-        }
-        printToScreen(input);
-      }
-    }
+	var keyCode = event.which || event.keyCode;
+	var input;
+	if(keyCode != 0) {
+		if(!((keyCode <= 57) && (keyCode >= 48)) && !CHAR_CODE.includes(keyCode)) {
+			event.preventDefault();
+		} else {
+			if((keyCode <= 57) && (keyCode >= 48)) {
+				input = String.fromCharCode(keyCode);
+			} else {
+				switch(keyCode) {
+					case 43:
+						input = "+";
+						break;
+					case 45:
+						input = "-";
+						break;
+					case 42:
+						input = "×";
+						break;
+					case 47:
+						input = "÷";
+						break;
+					case 40:
+						input = "(";
+						break;
+					case 41:
+						input = ")";
+						break;
+					case 69:
+						input = "E";
+						break;
+					case 13: //enter
+					case 61:
+						input = "=";
+						break;
+					case 8: // backspace
+						input = "<-";
+						break;
+					case 46:
+						input = ".";
+						break;
+					case 37:
+						input = "%";
+						break;
+					case 94:
+						input = "^";
+						break;
+					case 8730:
+						input = "√(";
+						break;
+				}
+			}
+			printToScreen(input);
+		}
+	}
 }
 
 var all = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "+", "-", "×", "÷", "=", "C", "^", "^2", "E", "(", ")", "%", "√(", ".", "MR", "MS", "M+", "M-", "MC", "<-"];
@@ -117,157 +124,158 @@ var dot = 0;
 // Require: N/A
 // Update: equation, #current-input, #history-container
 // Return: N/A
-function printToScreen(input){
-  var equation = document.getElementById("equation-container").value;
-  var last = equation.slice(-1);
-  var last2 = equation.slice(-2);
-  var twoBefore = equation.charAt(equation.length - 2);
-  var threeBefore = equation.charAt(equation.length - 3);
-  var cutLast = equation.substring(0, equation.length - 1);
-  var cutLast2 = equation.substring(0, equation.length - 2);
-  var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-  var nochange = ((equation == "0") && [")", "<-"].includes(input)) ||
-                  (operation.includes(last) && input == ".") ||
-                  (last == "(" && (endOp.includes(input) || midOp.includes(input))) ||
-                  (input == "%" && (startOp.includes(last) || midOp.includes(last))) ||
-                  ((input == "^" || input == "^2") && (last == "%")) ||
-                  ((endOp.includes(input) || midOp.includes(input)) && last == "-" && twoBefore == "");
+function printToScreen(input) {
+	var equation = document.getElementById("equation-container").value;
+	var last = equation.slice(-1);
+	var last2 = equation.slice(-2);
+	var twoBefore = equation.charAt(equation.length - 2);
+	var threeBefore = equation.charAt(equation.length - 3);
+	var cutLast = equation.substring(0, equation.length - 1);
+	var cutLast2 = equation.substring(0, equation.length - 2);
+	var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+	var nochange = ((equation == "0") && [")", "<-"].includes(input)) ||
+		(operation.includes(last) && input == ".") ||
+		(last == "(" && (endOp.includes(input) || midOp.includes(input))) ||
+		(input == "%" && (startOp.includes(last) || midOp.includes(last))) ||
+		((input == "^" || input == "^2") && (last == "%")) ||
+		((endOp.includes(input) || midOp.includes(input)) && last == "-" && twoBefore == "");
 
-  if (!nochange){
-    id = "";
-    switch(input){
-      case "0":
-      case "1":
-      case "2":
-      case "3":
-      case "4":
-      case "5":
-      case "6":
-      case "7":
-      case "8":
-      case "9":
-        if((last == "0" && !numbers.includes(twoBefore) && twoBefore != ".") || endOp.includes(last)){
-          equation = cutLast;
-        }
-        break;
+	if(!nochange) {
+		id = "";
+		switch(input) {
+			case "0":
+			case "1":
+			case "2":
+			case "3":
+			case "4":
+			case "5":
+			case "6":
+			case "7":
+			case "8":
+			case "9":
+				if((last == "0" && !numbers.includes(twoBefore) && twoBefore != ".") || endOp.includes(last)) {
+					equation = cutLast;
+				}
+				break;
 
-      case "+":
-      case "×":
-      case "÷":
-        if(["+", "-", "×", "÷", "^", "E", "."].includes(last) || last2 == "√("){
-          equation = cutLast;
-        }
-        break;
+			case "+":
+			case "×":
+			case "÷":
+				if(["+", "-", "×", "÷", "^", "E", "."].includes(last) || last2 == "√(") {
+					equation = cutLast;
+				}
+				break;
 
-      case "-":
-        if(["+", "-", "×", "÷", "^", "."].includes(last) || (last == "0" && twoBefore == "")){
-          equation = cutLast;
-        }
-        break;
+			case "-":
+				if(["+", "-", "×", "÷", "^", "."].includes(last) || (last == "0" && twoBefore == "")) {
+					equation = cutLast;
+				}
+				break;
 
-      case "%":
-        if(["+", "-", "×", "÷", "^", "E", "(", ".", "%"].includes(last)){
-          equation = cutLast;
-        }
-        break;
+			case "%":
+				if(["+", "-", "×", "÷", "^", "E", "(", ".", "%"].includes(last)) {
+					equation = cutLast;
+				}
+				break;
 
-      case "^":
-      case "^2":
-        if(["+", "-", "×", "÷", "E", "(", "."].includes(last)){
-          equation = cutLast;
-        }
-        break;
+			case "^":
+			case "^2":
+				if(["+", "-", "×", "÷", "E", "(", "."].includes(last)) {
+					equation = cutLast;
+				}
+				break;
 
-      case ".":
-        if(["%", ")"].includes(last) || last2 == "√("){
-          equation = cutLast;
-        }
-        break;
+			case ".":
+				if(["%", ")"].includes(last) || last2 == "√(") {
+					equation = cutLast;
+				}
+				break;
 
-      case "√(":
-        if(["","."].includes(last)){
-          equation = cutLast;
-        }
-        break;
+			case "√(":
+				if(["", "."].includes(last)) {
+					equation = cutLast;
+				}
+				break;
 
-      case "(":
-        if(["."].includes(last) || (last == "0" && twoBefore == "")){
-          equation = cutLast;
-        }
-        break;
+			case "(":
+				if(["."].includes(last) || (last == "0" && twoBefore == "")) {
+					equation = cutLast;
+				}
+				break;
 
-      case ")":
-        if(["+", "-", "×", "÷", "^", "E", "."].includes(last) || last2 == "√("){
-          equation = cutLast;
-        }
-        break;
+			case ")":
+				if(["+", "-", "×", "÷", "^", "E", "."].includes(last) || last2 == "√(") {
+					equation = cutLast;
+				}
+				break;
 
-      case "E":
-        if (!numbers.includes(last)){
-          equation = cutLast;
-        }
-        break;
+			case "E":
+				if(!numbers.includes(last)) {
+					equation = cutLast;
+				}
+				break;
 
-      case "<-":
-        if(last2 == "√("){
-          equation = cutLast2;
-        } else {
-          equation = cutLast;
-        }
-        if(last == "."){
-          dot = 0;
-        }
-        if(last == "^"){
-          setexp = 0;
-        }
-        break;
+			case "<-":
+				if(last2 == "√(") {
+					equation = cutLast2;
+				} else {
+					equation = cutLast;
+				}
+				if(last == ".") {
+					dot = 0;
+				}
+				if(last == "^") {
+					setexp = 0;
+				}
+				break;
 
-      case "C":
-        equation = "0";
-        break;
+			case "C":
+				equation = "0";
+				break;
 
-      case "MR":
-      case "MS":
-      case "M+":
-      case "M-":
-      case "MC":
-      case "=":
-        if(["+", "-", "×", "÷", "^", "E", ".", "("].includes(last)  || last2 == "√("){
-          equation = cutLast;
-        }
-        equation += "=";
-        // result = evaluate equation
-        // addHistory(equation, result);
-        equation = "0";
-        update(equation);
-        setexp = 0;
-        dot = 0;
-        break;
-      }
-    if(!clear.includes(input) && input != "<-"){
-      if(!(input == "^2" || input == "^") && input != "."){
-        equation += input;
-        if (["+", "-", "×", "÷", "(", ")"].includes(input)){
-          setexp = 0;
-        }
-        if (["+", "-", "×", "÷", "(", "^"].includes(input)){
-          dot = 0;
-        }
-      } else{
-        if(setexp == 0 && (input == "^2" || input == "^")){
-          setexp = 1;
-          dot = 0;
-          equation += input;
-        } else if (input == "." && dot == 0){
-          dot = 1;
-          equation += input;
-        }
-      }
-    }
+			case "MR":
+			case "MS":
+			case "M+":
+			case "M-":
+			case "MC":
+			case "=":
+				if(["+", "-", "×", "÷", "^", "E", ".", "("].includes(last) || last2 == "√(") {
+					equation = cutLast;
+				}
+				equation += "=";
+				// result = evaluate equation
+				// addHistory(equation, result);
+				addHistory(equation, "5")
+				equation = "0";
+				update(equation);
+				setexp = 0;
+				dot = 0;
+				break;
+		}
+		if(!clear.includes(input) && input != "<-") {
+			if(!(input == "^2" || input == "^") && input != ".") {
+				equation += input;
+				if(["+", "-", "×", "÷", "(", ")"].includes(input)) {
+					setexp = 0;
+				}
+				if(["+", "-", "×", "÷", "(", "^"].includes(input)) {
+					dot = 0;
+				}
+			} else {
+				if(setexp == 0 && (input == "^2" || input == "^")) {
+					setexp = 1;
+					dot = 0;
+					equation += input;
+				} else if(input == "." && dot == 0) {
+					dot = 1;
+					equation += input;
+				}
+			}
+		}
 
-    if(equation === ""){
-      equation += "0";
-    }
-    update(equation);
-  }
+		if(equation === "") {
+			equation += "0";
+		}
+		update(equation);
+	}
 }
