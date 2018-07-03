2c5136d6345dc01b9d916261fed3cec43066b2db
Yuxiang Lin <lin.2453@osu.edu>
2018-07-03 18:11:33 -0400

    Merge with gail

--- js/index.js
+++ js/index.js
@@ -26,18 +26,3 @@ clearBtn.addEventListener("click",clearHistory,false);
 //this.parentNode.parentNode.removeChild(this.parentNode);
 //}, false);
 //};
-//
-<<<<<<< HEAD
-=======
-
-var backBtn = document.getElementsByClassName('fa-backspace');
-backBtn[0].addEventListener('click', function(){
-var equation = document.getElementById("equation-container");
-if (equation.value != undefined){
-    equation.value= equation.value.substring(0, equation.value.length - 1);
-    if (equation.value==''){
-        equation.value=0;
-    }
-}
-}, false); 
->>>>>>> 95d1e943649c171927ed8bf75f6b06303d08ef03
