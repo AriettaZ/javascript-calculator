f96a2033c30807e2bd2aa3226f7fec715b68e79d
ariel <zhu.1444@osu.edu>
2018-07-04 14:07:04 -0400

    add template

---
+++ test/unit_testing_tokenize.js
@@ -0,0 +1,39 @@
+describe("tokenize", function() {
+
+  beforeEach(function() {
+  });
+
+  it("", function() {
+
+  });
+
+  describe("", function() {
+    beforeEach(function() {
+
+    });
+
+    it("", function() {
+
+    });
+
+    it("", function() {
+
+    });
+  });
+
+  it("", function() {
+
+  });
+
+  it("", function() {
+
+  });
+
+  //demonstrates use of expected exceptions
+  describe("", function() {
+    it("", function() {
+      // expect(function() {
+      // }).toThrowError("");
+    });
+  });
+});
---
+++ test/unit_testing_view.js
@@ -0,0 +1,39 @@
+describe("view", function() {
+
+  beforeEach(function() {
+  });
+
+  it("", function() {
+
+  });
+
+  describe("", function() {
+    beforeEach(function() {
+
+    });
+
+    it("", function() {
+
+    });
+
+    it("", function() {
+
+    });
+  });
+
+  it("", function() {
+
+  });
+
+  it("", function() {
+
+  });
+
+  //demonstrates use of expected exceptions
+  describe("", function() {
+    it("", function() {
+      // expect(function() {
+      // }).toThrowError("");
+    });
+  });
+});
