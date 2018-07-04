22e9e32344c6d147c8f4dd24ae1c547c575555b2
ariel <zhu.1444@osu.edu>
2018-07-04 18:29:57 -0400

    add testcases

--- test/unit_testing_updateMemory.js
+++ test/unit_testing_updateMemory.js
@@ -1,39 +1,53 @@
 describe("updateMemory", function() {
+  // var equation, memory, result;
+  // beforeEach(function() {
+  //   memory= 30;
+  //   equation = 80;
+  //   result = 100;
+  //   var dummyMemory = document.createElement('div');
+  //   dummyMemory.setAttribute("id", "current-memory");
+  //   document.getElementById = jasmine.createSpy('HTML Element').and.returnValue(dummyMemory);
+  //   var dummyEquation= document.createElement('input');
+  //   dummyEquation.setAttribute("id", "equation-container");
+  //   dummyEquation.setAttribute("val", "3243253242");
+  //   dummyEquation.setAttribute("placeholder", "3243253242");
+  //   document.getElementById =jasmine.createSpy('HTML Element').and.returnValue(dummyEquation);
+  // });
+  //
+  // it("should handle MS", function() {
+  //   this.memory = 1000;
+  //   this.equation = 800;
+  //   this.result = 900;
+  //   var global = (function(){ return this; })();
+  //   spyOn(global, handleMS());
+  //   expect(document.getElementById("current-memory").innerHTML).toEqual('3243253242');
+  // });
+  //
+  // it("should handle MR", function() {
+  //   // memory = jasmine.createSpy('HTML Element').and.returnValue(dummyMemory);
+  //   handleMR()
+  //   expect(document.getElementById("current-memory").innerHTML).toEqual('');
+  // });
+  //
+  // it("should handle MPlus", function() {
+  //   // memory = jasmine.createSpy('HTML Element').and.returnValue(dummyMemory);
+  //   handleMPlus();
+  //   expect(document.getElementById("current-memory").innerHTML).toEqual(80);
+  // });
 
-  beforeEach(function() {
-  });
+  // describe("handleMemory", function() {
+  //   beforeEach(function() {
+  //
+  //   });
+  //
+  //   it("should update e+ to E", function() {
+  //     updateMemory('2.34e+10')
+  //     expect(document.getElementById("current-memory").innerHTML).toEqual('2.34E10');
+  //   });
+  //
+  //   it("", function() {
+  //
+  //   });
+  // });
 
-  it("", function() {
-
-  });
-
-  describe("", function() {
-    beforeEach(function() {
-
-    });
-
-    it("", function() {
-
-    });
-
-    it("", function() {
-
-    });
-  });
-
-  it("", function() {
-
-  });
-
-  it("", function() {
-
-  });
-
-  //demonstrates use of expected exceptions
-  describe("", function() {
-    it("", function() {
-      // expect(function() {
-      // }).toThrowError("");
-    });
-  });
 });
